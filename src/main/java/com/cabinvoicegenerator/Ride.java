101364c162432c1319d75f558c7ee0fa60f734c2
MKS1998 <nksayyad2900@outlook.com>
2021-11-08 17:42:10 +0530

    Added : Program for the take multiple rides an calculate the total fare

---
+++ src/main/java/com/cabinvoicegenerator/Ride.java
@@ -0,0 +1,12 @@
+package com.cabinvoicegenerator;
+
+
+public class Ride {
+    public final int time;
+    public double distance;
+    //parameterized Constructor
+    public Ride(double distance, int time){
+        this.distance = distance;
+        this.time = time;
+    }
+}
